1546557ebcf7f38e61ad9726b1ba071c9250239a
zekang <zekangzhang@sf-express.com>
2018-12-20 14:46:38 +0800

    修改批量查询策略

--- utils/Strategy.php
+++ utils/Strategy.php
@@ -18,17 +18,17 @@ class Strategy
     const QUERY_TICKET_MULTI_MIN = 1;
 
     // 查询余票批量次数最大值
-    const QUERY_TICKET_MULTI_MAX = 100;
+    const QUERY_TICKET_MULTI_MAX = 20;
 
     // 查询余票失败等待时间
-    const QUERY_TICKET_FAILED_SECOND = 100;
+    const QUERY_TICKET_FAILED_SECOND = 50;
 
     public static function queryTicketMultiCount() {
         $ipLockTimes = Cache::get(Consts::CACHE_IP_LOCK_TIMES['key']) ?? 0;
         $multiCount = Cache::get(Consts::CACHE_QUERY_TICKET_MULTI_COUNT['key']) ?? self::QUERY_TICKET_MULTI_MAX;
 
         $ipLockTimes ++;
-        $multiCount = intval($multiCount / ($ipLockTimes + 1));
+        $multiCount = intval($multiCount / $ipLockTimes);
         $multiCount = ($multiCount > self::QUERY_TICKET_MULTI_MIN) ? $multiCount : self::QUERY_TICKET_MULTI_MIN;
 
         Cache::set(Consts::CACHE_QUERY_TICKET_MULTI_COUNT['key'], $multiCount, Consts::CACHE_QUERY_TICKET_MULTI_COUNT['ttl']);
